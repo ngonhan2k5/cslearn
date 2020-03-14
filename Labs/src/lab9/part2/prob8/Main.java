46373b8ed82561f658fedac3f83fd37bb6599e0e
NgoNhan <ngonhan2k5@gmail.com>
2020-03-13 22:51:00 -0600

    Finish 7,8

--- Labs/src/lab9/part2/prob8/Main.java
+++ Labs/src/lab9/part2/prob8/Main.java
@@ -1,6 +1,7 @@
 package lab9.part2.prob8;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 	List<OrderItem> orderItems = new ArrayList<>();
@@ -12,23 +13,28 @@ public class Main {
 	}
 	
 	private boolean findProduct(String prodName) {
-		for(OrderItem item : orderItems) {
-			if(item != null) {
-				Product p=item.getProduct();
-				if(p != null) {
-					String name = p.getProductName();
-					if(name != null) {
-						if(name.equals(prodName)) return true;
-					}
-				}
-			}
-		}
-		return false;
+
+//		for(OrderItem item : orderItems) {
+//			if(item != null) {
+//				Product p=item.getProduct();
+//				if(p != null) {
+//					String name = p.getProductName();
+//					if(name != null) {
+//						if(name.equals(prodName)) return true;
+//					}
+//				}
+//			}
+//		}
+//		return false;
+		return orderItems.stream()
+				.map(o -> Optional.of(o.getProduct()).orElse(new Product("", "", 0)))
+				.filter(p->prodName.equals(p.getProductName()))
+				.count() > 0;
 	}
 	
 	private void loadOrderItemData() {
 		orderItems.add(new OrderItem(new Product("1016", "Tools", 131.00), 3));
-		orderItems.add(new OrderItem(new Product("1017", "Fishing Rod", 111.00), 1));
+		orderItems.add(new OrderItem(new Product("1017", null, 111.00), 1));
 		orderItems.add(new OrderItem(new Product("1018", "Game of Go",66.00), 2));
 		orderItems.add(new OrderItem(new Product("1019", "Flowers", 221.00), 5));
 	}
